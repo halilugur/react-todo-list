865282f9d0053986bbe353c7e00c3d71671c8def
halilugur <mr.halilugur@gmail.com>
2021-04-24 20:38:45 +0300

    todo list development finished.

--- App.js
+++ App.js
@@ -6,107 +6,96 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
+import ItemList from './ItemList';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      data: [],
+    };
+  }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  render() {
+    const {text, data} = this.state;
+    return (
+      <SafeAreaView>
+        <View style={style.title}>
+          <Text style={style.title_text}>To-Do List</Text>
+        </View>
+        <View style={{padding: 10, flexDirection: 'row'}}>
+          <TextInput
+            style={style.input}
+            value={text}
+            onChangeText={text => this.setState({text})}
+          />
+          <TouchableOpacity onPress={this.handleSave} style={style.button}>
+            <Text style={style.title_text}>Ekle</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          {data.map((item, index) => {
+            return (
+              <ItemList
+                text={item.text}
+                id={index}
+                remove={this.handleRemove}
+              />
+            );
+          })}
+        </View>
+      </SafeAreaView>
+    );
+  }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  handleSave = () => {
+    const {text, data} = this.state;
+    if (text) {
+      data.push({text});
+      this.setState({data, text: ''});
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  handleRemove = index => {
+    const {data} = this.state;
+    if (index) {
+      data.splice(index, 1);
+      this.setState({data});
+    }
+  };
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const style = StyleSheet.create({
+  title: {
+    backgroundColor: 'red',
+    padding: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title_text: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  input: {
+    padding: 10,
+    backgroundColor: 'white',
+    flex: 1,
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    padding: 10,
+    backgroundColor: 'red',
+    flex: 0.3,
+    borderRadius: 5,
+    marginLeft: 10,
   },
 });
-
-export default App;
---
+++ ItemList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const ItemList = ({text, id, remove}) => {
+  return (
+    <View style={style.main}>
+      <View style={style.card}>
+        <Text style={{marginRight: 20}}>{id}</Text>
+        <Text>{text}</Text>
+      </View>
+      <View style={style.button}>
+        <TouchableOpacity onPress={remove}>
+          <Text
+            style={{textAlign: 'center', color: 'white', fontWeight: 'bold'}}>
+            Sil
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  main: {
+    flexDirection: 'row',
+  },
+  card: {
+    padding: 10,
+    backgroundColor: '#ddd',
+    margin: 5,
+    flex: 1,
+    flexDirection: 'row',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'red',
+    margin: 5,
+    flex: 0.2,
+    borderRadius: 5,
+  },
+});
+
+export default ItemList;
